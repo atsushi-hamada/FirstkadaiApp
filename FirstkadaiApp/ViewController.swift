5970eb93e60d90417e46ccb7fbd0ba10cc1a347a
atsushi.hamada <tomato8181@gmail.com>
2024-02-08 13:59:43 +0900

    Initial Commit

---
+++ FirstkadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstkadaiApp
+//
+//  Created by cpcadmin on 2024/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
